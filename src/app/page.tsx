87003be950a31325cd282262fc2b81a80d0043e9
taminororo <169162271+taminororo@users.noreply.github.com>
2025-06-04 00:11:50 +0900

    [add]ボタンコンポーネントにボタンの機能をモジュール化して移す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  )
-}
+import Button from '../../components/Button';
 
 export default function MyApp() {
   return (
     <div>
-      <h1>Welcom to my app</h1>
-      <MyButton />
+      <h1>Welcome to my app</h1>
+      <Button />
     </div>
   );
 }
