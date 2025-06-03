fc3aad6e212bde513ae961074ff6d43416e8098e
taminororo <169162271+taminororo@users.noreply.github.com>
2025-06-04 00:24:52 +0900

    [add]export default functionの中身だけが表示されることの確認。複数コンポーネントを使用したい場合は片方をnamed exportしてデフォルトエクスポートの中でimportして使えば表示されることの確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,16 @@ export default function MyApp() {
     <div>
       <h1>Welcome to my app</h1>
       <Button />
+      <App /> {/* 呼び出せば、Appコンポーネントも表示されます */}
+    </div>
+  );
+}
+
+export function App() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <Button />
     </div>
   );
 }
