81826756393ddd8727817e4649646449a86cfef2
taminororo <169162271+taminororo@users.noreply.github.com>
2025-06-04 11:49:23 +0900

    [add]キーボードコンポーネントの実装、FIgmaはShift+Dで開発者モード、v4からtailwind.config.jsがいらなくなることの発見

--- components/AlphabetButton.tsx
+++ components/AlphabetButton.tsx
@@ -1,6 +1,10 @@
 function AlphabetButton() {
   return (
-    <button>I'm a button</button>
+    <div className="p-2.5 bg-stone-300 rounded-sm inline-flex flex-col justify-center items-center gap-2.5">
+        <button className="text-black text-base font-normal font-sans">
+            Q
+        </button>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import AlphabetButton from '../../components/AlphabetButton';
 import Button from '../../components/Button';
+
 
 export default function MyApp() {
   return (
@@ -6,7 +8,12 @@ export default function MyApp() {
       <h1>Welcome to my app</h1>
       <Button />
       <App /> {/* 呼び出せば、Appコンポーネントも表示されます */}
+      <div>
+      <h1>アルファベット</h1>
+      <AlphabetButton />
+      </div>
     </div>
+    
   );
 }
 
